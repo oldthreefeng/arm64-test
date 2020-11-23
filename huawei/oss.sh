97496b47feb1673806239858296f161805209835
oldthreefeng <louisehong4168@gmail.com>
2020-11-23 14:35:37 +0800

    fix release
    Signed-off-by: oldthreefeng <louisehong4168@gmail.com>

--- huawei/oss.sh
+++ huawei/oss.sh
@@ -6,4 +6,4 @@ echo "oss://fyhy/arm64/$md5-$1/kube$1-arm64.tar.gz"
 
 sed -i s/VERSION/$1/g production.yml
 
-echo "marketctl create -f production.yml --token $MARKET_API_TOEKN"
+echo "marketctl create -f production.yml --token $MARKET_API_TOKEN"
--- huawei/package.sh
+++ huawei/package.sh
@@ -23,7 +23,7 @@ echo "install git"
 remotecmd 'yum install -y git conntrack'
 
 echo "clone cloud kernel"
-remotecmd 'git clone https://github.com/oldthreefeng/arm64-test'
+remotecmd 'git clone https://${GH_TOKEN}@github.com/oldthreefeng/arm64-test'
 
 echo "install kubernetes bin"
 remotecmd "cd arm64-test && \
