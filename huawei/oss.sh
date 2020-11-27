c5c6c64564fd1715ee0cc63f8d75d204ec30d8bc
oldthreefeng <louisehong4168@gmail.com>
2020-11-27 16:33:43 +0800

    fix
    Signed-off-by: oldthreefeng <louisehong4168@gmail.com>

--- huawei/oss.sh
+++ huawei/oss.sh
@@ -4,6 +4,6 @@ md5=$(md5sum /tmp/kube$1-arm64.tar.gz | awk  '{print $1}')
 echo $md5 && ossutil64 -c /tmp/oss-config cp /tmp/kube$1-arm64.tar.gz oss://fyhy/arm64/$md5-$1/kube$1-arm64.tar.gz
 echo "oss://fyhy/arm64/$md5-$1/kube$1-arm64.tar.gz"
 
-sed -i s/VERSION/$1/g production.yml
+sed -i s/VERSION/$1/g huawei/production.yml
 
 echo "marketctl create -f production.yml --token $MARKET_API_TOKEN"
