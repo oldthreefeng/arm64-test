6c2762000a2c9e4a1c4896516d6f69830aee153f
oldthreefeng <louisehong4168@gmail.com>
2020-12-06 19:15:18 +0800

    marketctl v1.0.3 release
    Signed-off-by: oldthreefeng <louisehong4168@gmail.com>

--- huawei/oss.sh
+++ huawei/oss.sh
@@ -5,7 +5,7 @@ echo $md5
 #echo $md5 && ossutil64 -c /tmp/oss-config cp /tmp/kube$1-arm64.tar.gz oss://fyhy/arm64/$md5-$1/kube$1-arm64.tar.gz
 #echo "oss://fyhy/arm64/$md5-$1/kube$1-arm64.tar.gz"
 
-wget https://sealyun-market.oss-accelerate.aliyuncs.com/marketctl/v1.0.2/linux_arm64/marketctl
+wget https://sealyun-market.oss-accelerate.aliyuncs.com/marketctl/v1.0.3/linux_arm64/marketctl
 chmod a+x marketctl
 cat > marketctl_$1.yaml << EOF
 market:
