d9ef87b6684a55a02382e06864590714441d90b4
oldthreefeng <louisehong4168@gmail.com>
2021-01-16 22:46:18 +0800

    fix crictl to my own server
    Signed-off-by: oldthreefeng <louisehong4168@gmail.com>

--- huawei/package.sh
+++ huawei/package.sh
@@ -29,9 +29,12 @@ echo "install kubernetes bin"
 remotecmd "cd arm64-test && \
            wget https://dl.k8s.io/v$1/kubernetes-server-linux-arm64.tar.gz && \
            wget https://github.com/sealyun-market/containerd/releases/download/v1.4.3/cri-containerd-cni-1.4.3-linux-arm64.tar.gz  && \
+           wget https://fenghong.tech/marketctl/crictl-v1.20.0-linux-arm64.tar.gz && \
            cp cri-containerd-cni-1.4.3-linux-arm64.tar.gz kube/containerd/cri-containerd-cni-linux-arm64.tar.gz && \
            tar zxvf kubernetes-server-linux-arm64.tar.gz && \
+           tar xf crictl-v1.20.0-linux-arm64.tar.gz && \
            cd kube && \
+           cp ../crictl bin/ && \
            cp ../kubernetes/server/bin/kubectl bin/ && \
            cp ../kubernetes/server/bin/kubelet bin/ && \
            cp ../kubernetes/server/bin/kubeadm bin/ && \
